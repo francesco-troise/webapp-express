fdd40d12f508d593c6ba1ccfb781ddbed293fb9c
FrancescoTroise <francesco.troise98@gmail.com>
2025-12-10 14:29:01 +0100

    Set up and configure Express Server
    Install the "mysql2" library
    
    Connect to the database

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+//Import di express
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//Import del file di connessione al database
+const db = require("./database/database_connection");
+
+//Definizione rotta radice
+app.get("/", (req, res) => {
+  console.log("Rotta radice");
+});
+
+//Running del server
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
