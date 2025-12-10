01a731193e79518c88f1cdfaeb5f0d6b5afc5308
FrancescoTroise <francesco.troise98@gmail.com>
2025-12-10 20:07:19 +0100

    add index route to get movie list

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -1,6 +1,12 @@
 const connection = require("../database/database_connection");
 
-const index = (req, res) => {};
+const index = (req, res) => {
+  const sql = "SELECT * FROM movies";
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(results);
+  });
+};
 
 const show = (req, res) => {};
 
