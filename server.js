e3ac117b305d5bc31278c7b030e9ff7a3d5dfa63
FrancescoTroise <francesco.troise98@gmail.com>
2025-12-13 14:56:04 +0100

    Add Middleware for Non-Existing Routes and Middleware for Error Handling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-//Import del middlewere per accesso al database
+//Import del middlewereCORS per accesso al database
 const corsMiddleware = require("./middleware/generic_middelware");
 
 //middleware per accesso al database
@@ -28,6 +28,26 @@ app.get("/", (req, res) => {
 
 //Import del file di connessione al database
 const db = require("./database/database_connection");
+
+// Middleware per rotte inesistenti(404)
+app.use((req, res, next) => {
+  //Invio dell'errore
+  res.status(404).json({
+    status: "error",
+    message: `La rotta non esiste`,
+  });
+});
+
+// Middleware per gestione errori
+app.use((err, req, res, next) => {
+  //log dell'errore
+  console.error(err.stack);
+  //Invio dell'errore
+  res.status(err.status || 500).json({
+    status: "error",
+    message: err.message || "Errore interno del server",
+  });
+});
 
 //Running del server
 app.listen(PORT, () => {
